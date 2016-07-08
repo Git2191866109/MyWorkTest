d86ce6a88229c3f752751215cef0c24c89a644ad
mawei <2191866109@qq.com>
2016-07-08 18:39:34 +0800

    java处理工作需求

--- worktest/src/main/java/com/mw/java/test/spark/SparkTest.java
+++ worktest/src/main/java/com/mw/java/test/spark/SparkTest.java
@@ -2,10 +2,13 @@ package com.mw.java.test.spark;
 
 import com.mw.java.test.Constant;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
+import scala.Tuple3;
 
 import java.util.List;
 
@@ -19,38 +22,87 @@ public class SparkTest {
      * 2、保存到151hdfs中
      * 3、拼接key
      * 4、从redis中获取这个key的数据，
-     * 			分两中情况：一，得到redis中原始数据，有重复，需要去重
-     * 					   二，redis中没有数据，然后到151Hdfs上去获取，规则：根据	USERID("用户id",0),
-     * 					   	   FROMTYPE("来源",2),获取，然后通过，KW("关键字",4)去重，并且得到topN
-     *
+     * 分两中情况：一，得到redis中原始数据，有重复，需要去重
+     * 二，redis中没有数据，然后到151Hdfs上去获取，规则：根据	USERID("用户id",0),
+     * FROMTYPE("来源",2),获取，然后通过，KW("关键字",4)去重，并且得到topN
+     * <p/>
      * 5、然后覆盖掉原来的redis中的数据
+     * USERID("用户id",0),
+     * MODERNIZED("设备id",1),
+     * FROMTYPE("来源",2),
+     * TYPE("类型",3),
+     * KW("关键字",4),
+     * CARSTYLEID("车型id",5),
+     * NUM("数量",6),
+     * TIMES("时间",7);
      */
     public static void main(String[] args) {
+        final String KEY_TOP = "ahisrd";
+//        String key = CommonUtil.KEY_TOP + CommonUtil.KEY_JOIN_TYPE + values[HisEnum.USERID.getValue()] + CommonUtil.KEY_JOIN_TYPE + values[HisEnum.FROMTYPE.getValue()];
         String path = "E:\\work_test\\MyWorkTest\\worktest\\src\\main\\resources\\source\\sparktext";
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("JavaWordCount");
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
         ctx.setLogLevel("OFF");
-        JavaRDD<String> lines = ctx.textFile(path);
+        JavaRDD<String> lines = ctx.textFile(path).distinct();
+        lines.cache();
+         /*去重*/
+//        JavaRDD<String> quchong = lines.distinct();
         List<String> linesResults = lines.collect();
-//        for (String s : linesResults) {
+        for (String s : linesResults) {
 //            System.out.println(s);
-//        }
-         /*将其组合成 (kw,（USERID_FROMTYPE，s))形式,然后按照key分组 */
-        JavaRDD<Tuple2<String,Tuple2<String,String>>> splitLine = lines.map(new Function<String, Tuple2<String,Tuple2<String,String>>>() {
+        }
+        /*call方法的返回类型是Tuple2<String, Tuple3<String,String, String>>，对应的Function的第二个参数就是call方法的返回类型*/
+        JavaRDD<Tuple2<String, Tuple3<String, String, String>>> splitLine = lines.map(new Function<String, Tuple2<String, Tuple3<String, String, String>>>() {
             @Override
-            public Tuple2<String,Tuple2<String,String>> call(String v1) throws Exception {
-                String [] splitResut = v1.split(Constant.HDFSSPLIT);
+            public Tuple2<String, Tuple3<String, String, String>> call(String v1) throws Exception {
+                String[] splitResut = v1.split(Constant.HDFSSPLIT);
                 String userid = splitResut[0].trim();
                 String fromtype = splitResut[2].trim();
                 String kw = splitResut[4].trim();
-                String userid_fromtype = userid+Constant._+fromtype;
-                return new Tuple2<String,Tuple2<String,String>>(userid_fromtype,new Tuple2<String, String>(v1,kw));
+                /*这里的new对象就是JavaRDD的返回类型，也是call方法的返回类型*/
+                return new Tuple2<String, Tuple3<String, String, String>>(kw, new Tuple3<String, String, String>(userid, fromtype, v1));
             }
         });
-        List<Tuple2<String,Tuple2<String,String>>> wordsResults = splitLine.collect();
-        for (Object s : wordsResults) {
-            System.out.println(s.toString());
-        }
+//        List<Tuple2<String, Tuple3<String, String, String>>> wordsResults = splitLine.collect();
+//        for (Object s : wordsResults) {
+//            System.out.println(s.toString());
+//        }
+         /*将其组合成 (kw,（USERID,FROMTYPE，s))形式,然后按照key分组 ,java方式分组，这里面比较傻逼，我想要根据什么分组，我就返回什么就可以啦，额莫名其妙*/
+        JavaPairRDD<String, Iterable<Tuple2<String, Tuple3<String, String, String>>>> paris = splitLine.groupBy(new Function<Tuple2<String, Tuple3<String, String, String>>, String>() {
+            @Override
+            public String call(Tuple2<String, Tuple3<String, String, String>> tuple2) throws Exception {
+                return tuple2._1;
+            }
+        });
+        JavaRDD<Iterable<Tuple2<String, Tuple3<String, String, String>>>> secondMap = paris.values();
+        System.out.println(secondMap.collect().toString());
+//        secondMap.mapToPair(new PairFunction<Iterable<Tuple2<String,Tuple3<String,String,String>>>, Object, Object>() {
+//            @Override
+//            public Tuple2<Object, Object> call(Iterable<Tuple2<String, Tuple3<String, String, String>>> tuple2s) throws Exception {
+//                return null;
+//            }
+//        });
+
+         /*将其组合成 (kw,（USERID_FROMTYPE，s))形式,然后按照key分组 */
+//        JavaRDD<Tuple2<String, Tuple2<String, String>>> splitLine = lines.map(new Function<String, Tuple2<String, Tuple2<String, String>>>() {
+//            @Override
+//            public Tuple2<String, Tuple2<String, String>> call(String v1) throws Exception {
+//                String[] splitResut = v1.split(Constant.HDFSSPLIT);
+//                String userid = splitResut[0].trim();
+//                String fromtype = splitResut[2].trim();
+//                String kw = splitResut[4].trim();
+//                String userid_fromtype = userid + Constant._ + fromtype;
+//                return new Tuple2<String, Tuple2<String, String>>(userid_fromtype, new Tuple2<String, String>(v1, kw));
+//            }
+//        });
+
+
+//        List<Tuple2<String,Tuple2<String,String>>> wordsResults = splitLine.distinct().collect();
+//        for (Object s : wordsResults) {
+//            System.out.println(s.toString());
+//        }
+
+
 //        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
 //            @Override
 //            public Iterable<String> call(String s) {
