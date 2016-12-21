f08d343043c121d7cbcd497a96d0d3685004307d
wei.ma <2191866109@qq.com>
2016-12-21 14:52:57 +0800

    分割合并

--- worktest/src/main/java/com/mw/java/test/java/JavaFileTest.java
+++ worktest/src/main/java/com/mw/java/test/java/JavaFileTest.java
@@ -17,6 +17,14 @@ public class JavaFileTest {
         splitFileDemo(new File("D:\\wallpaper_161103.zip"), 3);
     }
 
+    /**
+     * 文件合并：
+     */
+    @Test
+    public void joinFile() {
+        joinFileDemo("D:\\wallpaper_161103_data1.zip", "D:\\wallpaper_161103_data2.zip", "D:\\wallpaper_161103_data3.zip");
+    }
+
     private static void splitFileDemo(File src, int m) {
         if (src.isFile()) {
             //获取文件的总长度
@@ -31,8 +39,7 @@ public class JavaFileTest {
                 in = new FileInputStream(src);
                 for (int i = 1; i <= m; i++) {
                     StringBuffer sb = new StringBuffer();
-                    sb.append(src.getParent()).append("\\").append(fileName)
-                            .append("_data").append(i).append(endName);
+                    sb.append(src.getParent()).append("\\").append(fileName).append("_data").append(i).append(endName);
                     System.out.println(sb.toString());
                     File file2 = new File(sb.toString());  //创建写文件的输出流 21
                     OutputStream out = new FileOutputStream(file2);
@@ -56,11 +63,6 @@ public class JavaFileTest {
                 }
             }
         }
-    }
-
-    @Test
-    public void joinFile() {
-        joinFileDemo("D:\\wallpaper_161103_data1.zip", "D:\\wallpaper_161103_data2.zip", "D:\\wallpaper_161103_data3.zip");
     }
 
     private static void joinFileDemo(String... src) {
@@ -98,8 +100,3 @@ public class JavaFileTest {
     }
 
 }
-
-
-//文件分割的方法（方法内传入要分割的文件路径以及要分割的份数） 2 { 3  { 4  5  6  8  10  11  12 try { 13 in = new FileInputStream(src); 14 for(int i = 1; i <= m; i++) { 15 StringBuffer sb = new StringBuffer; 16 sb.append(src.getParent).append("\\").append(fileName) 17 .append("_data").append(i).append(endName); 18 System.out.println(sb.toString); 19 File file2 = new File(sb.toString); 20 //创建写文件的输出流 21 OutputStream out = new FileOutputStream(file2); 22 int len = -1; 23 byte bytes = new byte[10*1024*1024]; 24 while((len = in.read(bytes))!=-1) { 25 out.write(bytes, 0, len); 26 if(file2.length > (l / m)) { 27 break; 28 } 29 } 30 out.close; 31 } 32 } catch (Exception e) { 33 e.printStackTrace; 34 } 35 finally { 36 try { 37 in.close; 38 } catch (IOException e) { 39 e.printStackTrace; 40 } 41 } 42 } 43 }
-//        Java合并文件的方法：
-//        1 //文件合并的方法（传入要合并的文件路径） 2
